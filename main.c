f28a57f155b7b74887d22cd95d0fea1e1a3bdbb3
Artem <ediskrad11111@gmail.com>
2023-03-29 09:56:23 +0300

    m

--- main.c
+++ main.c
@@ -3,5 +3,5 @@
 int main() {
     const char* filename = getFileName();
     const char* compressedFileName = getFileCompressedName();
-    Compress((char*)filename, compressedFileName);
+    Compress(filename, compressedFileName);
 }
